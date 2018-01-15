537a5ee67ebf51226a17c3bc8dd1cd367b991aaf
Jeffrey Gordon <jgordon.oakland@gmail.com>
2018-01-15 19:07:16 +0000

    new demomode

--- loadbar.js
+++ loadbar.js
@@ -1,23 +1,43 @@
 var Loader = function(demo) {
+    var loader = this;
     this.States = {};
     // *****************************************************************************************
     // LoadState State
     this.States.LoadState = function() {};
     this.States.LoadState.prototype = {
         preload: function() {
+            game.add.text(0, 0, "Fetching images for loadbar...")
             for (var i = 0; i < 60; i++) {
                 var frameSpacer = "";
                 if (i < 10) frameSpacer = "0";
                 var frameFileName = 'assets/frame_' + frameSpacer + i.toString() + '.gif';
                 game.load.image('frame' + i.toString(), frameFileName);
             }
+            
+
         },
         create: function() {
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVeritcally = true;
+            game.scale.refresh();
             game.loader = game.add.sprite(0, 0, 'frame0');
             game.loadState = 'initial';
             game.loadFrame = 0;
             //for demo purposes
-            if(demo)setTimeout(progress, 1000);
+            if (demo) {
+                $("#slider").slider({
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 10,
+                    slide: function(event, ui) {
+                        $("#slider-value").html(ui.value);
+                        loader.setLoadPercentage(ui.value)
+                    }
+                });
+
+                $("#slider-value").html($('#slider').slider('value'));
+            }
         },
         update: function() {
             if (game.loadState === 'initial') {
@@ -33,7 +53,7 @@ var Loader = function(demo) {
             else if (game.loadState === 'loading') {
                 game.loadFrame = 30 + Math.floor(game.loadPercentage / 10);
                 game.loader.loadTexture('frame' + game.loadFrame);
-                if(game.loadPercentage >= 100) game.loadState = 'done'
+                if (game.loadPercentage >= 100) game.loadState = 'done'
             }
             else if (game.loadState === 'done') {
                 if (game.loadFrame < 59) {
@@ -56,17 +76,4 @@ var Loader = function(demo) {
     var game = new Phaser.Game(720, 402, Phaser.CANVAS, 'loaderDiv', this.States.LoadState);
 };
 
-//var loader = new Loader(true);
-
-var progress = function() {
-    console.log(loader.getLoadPercentage())
-    if(loader.getLoadPercentage() < 100)
-    {
-        loader.setLoadPercentage(loader.getLoadPercentage()+1);
-        setTimeout(progress, 50);
-    } else
-    {
-        setTimeout(loader.destroy, 2000);
-    }
-};
-
+var loader = new Loader(true);
